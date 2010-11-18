d508ae56e1ee4124cef309a97039f8178f60409e
uhhaha <uhhaha@gmail.com>
2010-11-18 20:03:34 +0900

    project file

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject iGAS "1.0.0-SNAPSHOT"
+  :description "i-GAS Development"
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]
+		 [incanter "1.2.3"]
+                 [swingrepl "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.2.1"]
+                     [jline "0.9.94"]])
